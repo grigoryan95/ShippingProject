165d4b3d6606e5ea8b195caa1fed9d3e3f32387f
grigoryan95 <grigoryanandranik95@gmail.com>
2021-09-01 13:38:35 +0400

    New update2

--- src/app/right-container/only-floor/only-floor.component.ts
+++ src/app/right-container/only-floor/only-floor.component.ts
@@ -2,8 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-only-floor',
-  templateUrl: './only-floor.component.html',
-  styleUrls: ['./only-floor.component.scss']
+  template: `
+    <div class="floorText">
+      <span>All purchases are subject to our Terms and Conditions.</span>
+    </div>
+  `,
+  styles: [`
+    .floorText {
+      font-size: 10px;
+      line-height: 12px;
+      color: #9C9C9C;
+      display: flex;
+      justify-content: flex-end;
+      margin-top: 170px;
+      padding: 0 10px;
+    }
+  `]
 })
 export class OnlyFloorComponent {
 
--- src/app/right-container/order-summary/order-summary.component.ts
+++ src/app/right-container/order-summary/order-summary.component.ts
@@ -2,9 +2,33 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-order-summary',
-  templateUrl: './order-summary.component.html',
-  styleUrls: ['./order-summary.component.scss']
+  template: `
+    <div class="divOrder">
+      <span class="orderSummary">Order Summary</span>
+      <span class="editOrder">edit order</span>
+    </div>
+  `,
+  styles: [`
+    .orderSummary {
+      font-size: 18px;
+      color: #5A1094;
+    }
+
+    .editOrder {
+      font-size: 12px;
+      line-height: 14px;
+      text-decoration: underline;
+      color: #979797;
+    }
+
+    .divOrder {
+      display: flex;
+      justify-content: space-between;
+      padding-top: 24px;
+    }
+  `]
 })
 export class OrderSummaryComponent {
 
 }
+
